f682a32348baee0791b0fca6262e9f958291c2b2
Dima <dmitriy.kirakosyan@gmail.com>
2021-05-24 00:47:28 +0300

    Migrate SPM to binaries

--- Package@swift-5.3.swift
+++ Package@swift-5.3.swift
@@ -7,100 +7,19 @@ import PackageDescription
 
 let package = Package(
     name: "AppCenter",
-    defaultLocalization: "en",
-    platforms: [
-        .iOS(.v9),
-        .macOS(.v10_10),
-        .tvOS(.v11)
-    ],
     products: [
-        .library(
-            name: "AppCenterAnalytics",
-            type: .static,
-            targets: ["AppCenterAnalytics"]),
-        .library(
-            name: "AppCenterCrashes",
-            type: .static,
-            targets: ["AppCenterCrashes"]),
-        .library(
-            name: "AppCenterDistribute",
-            type: .static,
-            targets: ["AppCenterDistribute"]),
+        .library(name: "AppCenter", targets: ["AppCenter"]),
+        .library(name: "AppCenterAnalytics", targets: ["AppCenterAnalytics"]),
+        .library(name: "AppCenterCrashes", targets: ["AppCenterCrashes"]),
+        .library(name: "AppCenterDistribute", targets: ["AppCenterDistribute"])
     ],
     dependencies: [
-        .package(url: "https://github.com/microsoft/PLCrashReporter.git", .upToNextMinor(from: "1.8.0")),
     ],
     targets: [
-        .target(
+        .binaryTarget(
             name: "AppCenter",
-            path: "AppCenter/AppCenter",
-            exclude: ["Support"],
-            cSettings: [
-                .define("APP_CENTER_C_VERSION", to:"\"4.1.2\""),
-                .define("APP_CENTER_C_BUILD", to:"\"1\""),
-                .headerSearchPath("**"),
-            ],
-            linkerSettings: [
-                .linkedLibrary("z"),
-                .linkedLibrary("sqlite3"),
-                .linkedFramework("Foundation"),
-                .linkedFramework("SystemConfiguration"),
-                .linkedFramework("AppKit", .when(platforms: [.macOS])),
-                .linkedFramework("UIKit", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("CoreTelephony", .when(platforms: [.iOS, .macOS])),
-            ]
-        ),
-        .target(
-            name: "AppCenterAnalytics",
-            dependencies: ["AppCenter"],
-            path: "AppCenterAnalytics/AppCenterAnalytics",
-            exclude: ["Support"],
-            cSettings: [
-                .headerSearchPath("**"),
-                .headerSearchPath("../../AppCenter/AppCenter/**"),
-            ],
-            linkerSettings: [
-                .linkedFramework("Foundation"),
-                .linkedFramework("UIKit", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("AppKit", .when(platforms: [.macOS])),
-            ]
-        ),
-        .target(
-            name: "AppCenterCrashes",
-            dependencies: [
-                "AppCenter",
-                .product(name: "CrashReporter", package: "PLCrashReporter"),
-            ],
-            path: "AppCenterCrashes/AppCenterCrashes",
-            exclude: ["Support", "Internals/MSACCrashesBufferedLog.hpp"],
-            cSettings: [
-                .headerSearchPath("**"),
-                .headerSearchPath("../../AppCenter/AppCenter/**"),
-            ],
-            linkerSettings: [
-                .linkedFramework("Foundation"),
-                .linkedFramework("UIKit", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("AppKit", .when(platforms: [.macOS])),
-            ]
-        ),
-        .target(
-            name: "AppCenterDistribute",
-            dependencies: ["AppCenter"],
-            path: "AppCenterDistribute/AppCenterDistribute",
-            exclude: ["Support"],
-            resources: [
-                .process("Resources/AppCenterDistribute.strings"),
-            ],
-            cSettings: [
-                .headerSearchPath("**"),
-                .headerSearchPath("../../AppCenter/AppCenter/**"),
-            ],
-            linkerSettings: [
-                .linkedFramework("Foundation"),
-                .linkedFramework("SafariServices", .when(platforms: [.iOS])),
-                .linkedFramework("AuthenticationServices", .when(platforms: [.iOS])),
-                .linkedFramework("UIKit", .when(platforms: [.iOS])),
-            ]
+            url: "https://github.com/microsoft/appcenter-sdk-apple/releases/download/4.1.1/AppCenter-SDK-Apple-XCFramework-4.1.1.zip",
+            checksum: "7d5cef21ed6eb69f753d302a593b9557c95509dd3e9c2d147dc29749c8d35f20"
         )
     ]
 )
