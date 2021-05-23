e4d3585fdeee0034f7b26d72ff61ba9f59cda7dc
Dima <dmitriy.kirakosyan@gmail.com>
2021-05-24 02:06:27 +0300

    Remove old manifest

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
@@ -7,73 +7,34 @@ import PackageDescription
 
 let package = Package(
     name: "AppCenter",
-    platforms: [
-        .iOS(.v9),
-        .macOS(.v10_10),
-        .tvOS(.v11)
-    ],
     products: [
-        .library(
-            name: "AppCenterAnalytics",
-            type: .static,
-            targets: ["AppCenterAnalytics"]),
-        .library(
-            name: "AppCenterCrashes",
-            type: .static,
-            targets: ["AppCenterCrashes"])
+        .library(name: "AppCenter", targets: ["AppCenter"]),
+        .library(name: "AppCenterAnalytics", targets: ["AppCenterAnalytics"]),
+        .library(name: "AppCenterCrashes", targets: ["AppCenterCrashes"]),
+        .library(name: "AppCenterDistribute", targets: ["AppCenterDistribute"])
     ],
     dependencies: [
-        .package(url: "https://github.com/microsoft/plcrashreporter.git", .upToNextMinor(from: "1.8.0")),
     ],
     targets: [
-        .target(
+        .binaryTarget(
             name: "AppCenter",
-            path: "AppCenter/AppCenter",
-            exclude: ["Support"],
-            cSettings: [
-                .define("APP_CENTER_C_VERSION", to:"\"4.1.2\""),
-                .define("APP_CENTER_C_BUILD", to:"\"1\""),
-                .headerSearchPath("**"),
-            ],
-            linkerSettings: [
-                .linkedLibrary("z"),
-                .linkedLibrary("sqlite3"),
-                .linkedFramework("Foundation"),
-                .linkedFramework("SystemConfiguration"),
-                .linkedFramework("AppKit", .when(platforms: [.macOS])),
-                .linkedFramework("UIKit", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("CoreTelephony", .when(platforms: [.iOS, .macOS])),
-            ]
+            url: "https://github.com/DmitriyKirakosyan/mobile-center-sdk-ios/releases/download/v1.0/AppCenter.xcframework.zip",
+            checksum: "3f0094463c8f1f10823f8245a8fed459e788019cb8fdc45576d096a37e2e348d"
         ),
-        .target(
+        .binaryTarget(
             name: "AppCenterAnalytics",
-            dependencies: ["AppCenter"],
-            path: "AppCenterAnalytics/AppCenterAnalytics",
-            exclude: ["Support"],
-            cSettings: [
-                .headerSearchPath("**"),
-                .headerSearchPath("../../AppCenter/AppCenter/**"),
-            ],
-            linkerSettings: [
-                .linkedFramework("Foundation"),
-                .linkedFramework("UIKit", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("AppKit", .when(platforms: [.macOS])),
-            ]
+            url: "https://github.com/DmitriyKirakosyan/mobile-center-sdk-ios/releases/download/v1.0/AppCenterAnalytics.xcframework.zip",
+            checksum: "966e72ad06da674e06621ddc34993e30c462c26022c4b632ad9631aab09f422c"
         ),
-        .target(
+        .binaryTarget(
             name: "AppCenterCrashes",
-            dependencies: ["AppCenter", "CrashReporter"],
-            path: "AppCenterCrashes/AppCenterCrashes",
-            exclude: ["Support"],
-            cSettings: [
-                .headerSearchPath("**"),
-                .headerSearchPath("../../AppCenter/AppCenter/**"),
-            ],
-            linkerSettings: [
-                .linkedFramework("Foundation"),
-                .linkedFramework("UIKit", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("AppKit", .when(platforms: [.macOS])),
-            ]
+            url: "https://github.com/DmitriyKirakosyan/mobile-center-sdk-ios/releases/download/v1.0/AppCenterCrashes.xcframework.zip",
+            checksum: "2c20964d5f63ac1ddff64b7cca5b14620ebcc2f95900fa34a6ed01b4a73b8817"
+        ),
+        .binaryTarget(
+            name: "AppCenterDistribute",
+            url: "https://github.com/DmitriyKirakosyan/mobile-center-sdk-ios/releases/download/v1.0/AppCenterDistribute.xcframework.zip",
+            checksum: "9a4e8669e7340c3bb6d6d9bd1efa60aee49d455bfc288ea6112a6a4b88a7f0c7"
         )
     ]
 )
