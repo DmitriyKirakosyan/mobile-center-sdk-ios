51623db8b778755f16ee95b94cf5ac90d52bb88e
Dima <dmitriy.kirakosyan@gmail.com>
2021-05-24 01:52:23 +0300

    distribute xcframeworks separately

--- Package@swift-5.3.swift
+++ Package@swift-5.3.swift
@@ -9,14 +9,32 @@ let package = Package(
     name: "AppCenter",
     products: [
         .library(name: "AppCenter", targets: ["AppCenter"])
+        .library(name: "AppCenterAnalytics", targets: ["AppCenterAnalytics"])
+        .library(name: "AppCenterCrashes", targets: ["AppCenterCrashes"])
+        .library(name: "AppCenterDistribute", targets: ["AppCenterDistribute"])
     ],
     dependencies: [
     ],
     targets: [
         .binaryTarget(
             name: "AppCenter",
-            url: "https://github.com/microsoft/appcenter-sdk-apple/releases/download/4.1.1/AppCenter-SDK-Apple-XCFramework-4.1.1.zip",
-            checksum: "3dd421f36c1b423d4637d854cbcfb973104c1cb3148419dd142bdadff5362682"
+            url: "https://github.com/DmitriyKirakosyan/mobile-center-sdk-ios/releases/download/v1.0/AppCenter.xcframework.zip",
+            checksum: "3f0094463c8f1f10823f8245a8fed459e788019cb8fdc45576d096a37e2e348d"
+        )
+        .binaryTarget(
+            name: "AppCenterAnalytics",
+            url: "https://github.com/DmitriyKirakosyan/mobile-center-sdk-ios/releases/download/v1.0/AppCenterAnalytics.xcframework.zip",
+            checksum: "966e72ad06da674e06621ddc34993e30c462c26022c4b632ad9631aab09f422c"
+        )
+        .binaryTarget(
+            name: "AppCenterCrashes",
+            url: "https://github.com/DmitriyKirakosyan/mobile-center-sdk-ios/releases/download/v1.0/AppCenterCrashes.xcframework.zip",
+            checksum: "2c20964d5f63ac1ddff64b7cca5b14620ebcc2f95900fa34a6ed01b4a73b8817"
+        )
+        .binaryTarget(
+            name: "AppCenterDistribute",
+            url: "https://github.com/DmitriyKirakosyan/mobile-center-sdk-ios/releases/download/v1.0/AppCenterDistribute.xcframework.zip",
+            checksum: "9a4e8669e7340c3bb6d6d9bd1efa60aee49d455bfc288ea6112a6a4b88a7f0c7"
         )
     ]
 )
